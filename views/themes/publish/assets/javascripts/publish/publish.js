937c1a46b8aad2d85be6d37e273ba7aeb80030c9
jason weng <wengjianm@gmail.com>
2016-06-20 17:30:37 +0800

    adjust action button layout

--- views/themes/publish/assets/javascripts/publish/publish.js
+++ views/themes/publish/assets/javascripts/publish/publish.js
@@ -148,7 +148,7 @@
           '<p class="hint">[[modalHint]]</p>' +
           '<div class="qor-field__datetimepicker qor-publish__datetimepicker">' +
             '<div class="mdl-textfield mdl-js-textfield">' +
-              '<input class="mdl-textfield__input qor-publish__time" id="qorPublishTime" type="text" placeholder="[[datetimepickerPlaceholder]]" />' +
+              '<input class="mdl-textfield__input qor-publish__time" id="qorPublishTime" type="text" placeholder="2010-12-01 00:00" />' +
               '<label class="mdl-textfield__label" for="qorPublishTime"></label>' +
             '</div>' +
             '<div>' +
